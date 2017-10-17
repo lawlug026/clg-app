eb1323a2be0fa6185d66fe6361ddd99831888e22
lawlug <ujwalgoyal.2@gmail.com>
2017-10-17 19:30:02 +0530

    Change in database

--- cbpgec.sql
+++ cbpgec.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb4
+-- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Oct 17, 2017 at 05:36 PM
--- Server version: 5.7.19-0ubuntu0.17.04.1
--- PHP Version: 7.0.22-0ubuntu0.17.04.1
+-- Host: 127.0.0.1
+-- Generation Time: Oct 17, 2017 at 01:38 PM
+-- Server version: 5.7.18-log
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,334 +23,15 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ass0`
---
-
-CREATE TABLE `ass0` (
-  `ques` varchar(255) DEFAULT NULL,
-  `op1` char(20) DEFAULT NULL,
-  `op2` char(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ass0`
---
-
-INSERT INTO `ass0` (`ques`, `op1`, `op2`) VALUES
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ass1`
---
-
-CREATE TABLE `ass1` (
-  `ques` varchar(255) DEFAULT NULL,
-  `op1` char(20) DEFAULT NULL,
-  `op2` char(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ass1`
---
-
-INSERT INTO `ass1` (`ques`, `op1`, `op2`) VALUES
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ass2`
---
-
-CREATE TABLE `ass2` (
-  `ques` varchar(255) DEFAULT NULL,
-  `op1` char(20) DEFAULT NULL,
-  `op2` char(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ass2`
---
-
-INSERT INTO `ass2` (`ques`, `op1`, `op2`) VALUES
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ass3`
---
-
-CREATE TABLE `ass3` (
-  `ques` varchar(255) DEFAULT NULL,
-  `op1` char(20) DEFAULT NULL,
-  `op2` char(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ass3`
---
-
-INSERT INTO `ass3` (`ques`, `op1`, `op2`) VALUES
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ass4`
---
-
-CREATE TABLE `ass4` (
-  `ques` varchar(255) DEFAULT NULL,
-  `op1` char(20) DEFAULT NULL,
-  `op2` char(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ass4`
---
-
-INSERT INTO `ass4` (`ques`, `op1`, `op2`) VALUES
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ass5`
---
-
-CREATE TABLE `ass5` (
-  `ques` varchar(255) DEFAULT NULL,
-  `op1` char(20) DEFAULT NULL,
-  `op2` char(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ass5`
---
-
-INSERT INTO `ass5` (`ques`, `op1`, `op2`) VALUES
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ass6`
---
-
-CREATE TABLE `ass6` (
-  `ques` varchar(255) DEFAULT NULL,
-  `op1` char(20) DEFAULT NULL,
-  `op2` char(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ass6`
---
-
-INSERT INTO `ass6` (`ques`, `op1`, `op2`) VALUES
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ass7`
---
-
-CREATE TABLE `ass7` (
-  `ques` varchar(255) DEFAULT NULL,
-  `op1` char(20) DEFAULT NULL,
-  `op2` char(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ass7`
---
-
-INSERT INTO `ass7` (`ques`, `op1`, `op2`) VALUES
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ass8`
---
-
-CREATE TABLE `ass8` (
-  `ques` varchar(255) DEFAULT NULL,
-  `op1` char(20) DEFAULT NULL,
-  `op2` char(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ass8`
---
-
-INSERT INTO `ass8` (`ques`, `op1`, `op2`) VALUES
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ass9`
---
-
-CREATE TABLE `ass9` (
-  `ques` varchar(255) DEFAULT NULL,
-  `op1` char(20) DEFAULT NULL,
-  `op2` char(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ass9`
---
-
-INSERT INTO `ass9` (`ques`, `op1`, `op2`) VALUES
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ass10`
---
-
-CREATE TABLE `ass10` (
-  `ques` varchar(255) DEFAULT NULL,
-  `op1` char(20) DEFAULT NULL,
-  `op2` char(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ass10`
---
-
-INSERT INTO `ass10` (`ques`, `op1`, `op2`) VALUES
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ass11`
---
-
-CREATE TABLE `ass11` (
-  `ques` varchar(255) DEFAULT NULL,
-  `op1` char(20) DEFAULT NULL,
-  `op2` char(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ass11`
---
-
-INSERT INTO `ass11` (`ques`, `op1`, `op2`) VALUES
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `assignment`
 --
 
 CREATE TABLE `assignment` (
-  `assid` varchar(20) DEFAULT NULL,
+  `assid` varchar(20) NOT NULL,
   `date` varchar(20) NOT NULL,
   `heading` varchar(20) NOT NULL,
   `ans` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `assignment`
---
-
-INSERT INTO `assignment` (`assid`, `date`, `heading`, `ans`) VALUES
-('ass1234561', '12-10-2017', 'abc', 'q1'),
-('ass1234561', '12-10-2017', 'abc', 'q1'),
-('ass1234561', '12-10-2017', 'abc', 'q1'),
-('ass1234561', '12-10-2017', 'abc', 'q1'),
-('ass12345cvgj61', '12-10-2017', 'abc', 'q1'),
-('ass12345cvgj61', '12-10-2017', 'abc', 'q1'),
-('ass12345cvgj61', '12-10-2017', 'abc', 'q1'),
-('ass12345cvgj61', '12-10-2017', 'abc', 'q1'),
-('ass12345cvgj61', '12-10-2017', 'abc', 'q1'),
-('ass12345cvgj61', '12-10-2017', 'abc', 'q1'),
-('ass', '12-10-2017', 'abc', 'q1'),
-('ass', '12-10-2017', 'abc', 'q1'),
-('ass1', '12-10-2017', 'abc', 'q1'),
-('assundefined1', '12-10-2017', 'abc', 'q1'),
-('ass0', '12-10-2017', 'abc', 'q1'),
-('ass0', '12-10-2017', 'abc', 'q1'),
-('ass1', '12-10-2017', 'abc', 'q1'),
-('ass2', '12-10-2017', 'abc', 'q1'),
-('ass3', '12-10-2017', 'abc', 'q1'),
-('ass4', '12-10-2017', 'abc', 'q1'),
-('ass5', '12-10-2017', 'abc', 'q1'),
-('ass6', '12-10-2017', 'abc', 'q1'),
-('ass7', '12-10-2017', 'abc', 'q1'),
-('ass8', '12-10-2017', 'abc', 'q1'),
-('ass9', '12-10-2017', 'abc', 'q1'),
-('ass10', '12-10-2017', 'abc', 'q1'),
-('ass11', '12-10-2017', 'abc', 'q1');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `assundefined1`
---
-
-CREATE TABLE `assundefined1` (
-  `ques` varchar(255) DEFAULT NULL,
-  `op1` char(20) DEFAULT NULL,
-  `op2` char(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `assundefined1`
---
-
-INSERT INTO `assundefined1` (`ques`, `op1`, `op2`) VALUES
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13'),
-('jhsdb', '12', '13');
 
 -- --------------------------------------------------------
 
@@ -360,17 +41,33 @@ INSERT INTO `assundefined1` (`ques`, `op1`, `op2`) VALUES
 
 CREATE TABLE `bearer` (
   `bear` varchar(30) NOT NULL,
-  `roll` varchar(11) NOT NULL,
-  `name` varchar(11) NOT NULL
+  `roll` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `bearer`
+-- Table structure for table `form`
 --
 
-INSERT INTO `bearer` (`bear`, `roll`, `name`) VALUES
-('12', '1', 'ujjwal'),
-('13', '1', 'ujjwal');
+CREATE TABLE `form` (
+  `roll` int(20) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `semester` int(10) DEFAULT NULL,
+  `fatherName` varchar(50) DEFAULT NULL,
+  `motherName` varchar(50) DEFAULT NULL,
+  `studentMobile` int(15) DEFAULT NULL,
+  `fatherMobile` int(15) DEFAULT NULL,
+  `fatherOccupation` varchar(100) DEFAULT NULL,
+  `motherOccupation` varchar(100) DEFAULT NULL,
+  `fatherOfficeAddress` varchar(100) DEFAULT NULL,
+  `address1` varchar(100) DEFAULT NULL,
+  `address2` varchar(100) DEFAULT NULL,
+  `dob` varchar(10) DEFAULT NULL,
+  `trainingDet` varchar(100) DEFAULT NULL,
+  `acheivementDet` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -379,126 +76,20 @@ INSERT INTO `bearer` (`bear`, `roll`, `name`) VALUES
 --
 
 CREATE TABLE `logindata` (
-  `roll` int(20) DEFAULT NULL,
+  `roll` int(20) NOT NULL,
   `password` varchar(20) DEFAULT NULL,
-  `token` varchar(120) DEFAULT NULL
+  `name` varchar(120) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `logindata`
 --
 
-INSERT INTO `logindata` (`roll`, `password`, `token`) VALUES
+INSERT INTO `logindata` (`roll`, `password`, `name`) VALUES
 (1, 'ujjwal', '12'),
 (2, 'pankaj', NULL),
 (3, 'abhay', '12'),
 (78945, 'jknitsbjb', NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `maincounter`
---
-
-CREATE TABLE `maincounter` (
-  `number` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `maincounter`
---
-
-INSERT INTO `maincounter` (`number`) VALUES
-(15),
-(16),
-(17),
-(18),
-(19),
-(20),
-(21),
-(22),
-(23),
-(24),
-(25),
-(26),
-(27),
-(28),
-(29),
-(30),
-(31),
-(32),
-(33),
-(34),
-(35),
-(36),
-(37),
-(38),
-(39),
-(40),
-(41),
-(42),
-(43),
-(44),
-(45),
-(46),
-(47),
-(48),
-(49),
-(50),
-(51),
-(52),
-(53),
-(54),
-(55),
-(56),
-(57),
-(58),
-(59),
-(60),
-(61),
-(62),
-(63),
-(64),
-(65),
-(66),
-(67),
-(68),
-(69),
-(70),
-(71),
-(72),
-(73),
-(74),
-(75),
-(76),
-(77),
-(78),
-(79),
-(80),
-(81),
-(82),
-(83),
-(84),
-(85),
-(86),
-(87),
-(88),
-(89),
-(90),
-(91),
-(92),
-(93),
-(94),
-(95),
-(96),
-(97),
-(98),
-(99),
-(100),
-(101),
-(102),
-(103),
-(104);
 
 -- --------------------------------------------------------
 
@@ -513,71 +104,6 @@ CREATE TABLE `notice` (
   `content` varchar(500) NOT NULL,
   `file` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `queue01`
---
-
-CREATE TABLE `queue01` (
-  `number` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `queue01`
---
-
-INSERT INTO `queue01` (`number`) VALUES
-(2),
-(3),
-(4),
-(5),
-(6),
-(7),
-(8),
-(9),
-(10),
-(11),
-(12),
-(13);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `queue02`
---
-
-CREATE TABLE `queue02` (
-  `number` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `queue02`
---
-
-INSERT INTO `queue02` (`number`) VALUES
-(14);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `queue03`
---
-
-CREATE TABLE `queue03` (
-  `number` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `queue04`
---
-
-CREATE TABLE `queue04` (
-  `number` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -614,62 +140,33 @@ CREATE TABLE `teaches` (
   `subId` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `counter` varchar(2000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`username`, `password`, `counter`) VALUES
-('admin', '12345', 'maincounter'),
-('user1', '1234', 'queue01'),
-('user2', '1234', 'queue02'),
-('user3', '1234', 'queue03'),
-('user4', '1234', 'queue04');
-
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `maincounter`
+-- Indexes for table `assignment`
 --
-ALTER TABLE `maincounter`
-  ADD PRIMARY KEY (`number`);
+ALTER TABLE `assignment`
+  ADD PRIMARY KEY (`assid`);
 
 --
--- Indexes for table `queue01`
+-- Indexes for table `bearer`
 --
-ALTER TABLE `queue01`
-  ADD PRIMARY KEY (`number`);
+ALTER TABLE `bearer`
+  ADD PRIMARY KEY (`bear`);
 
 --
--- Indexes for table `queue02`
+-- Indexes for table `form`
 --
-ALTER TABLE `queue02`
-  ADD PRIMARY KEY (`number`);
+ALTER TABLE `form`
+  ADD PRIMARY KEY (`roll`);
 
 --
--- Indexes for table `queue03`
+-- Indexes for table `logindata`
 --
-ALTER TABLE `queue03`
-  ADD PRIMARY KEY (`number`);
-
---
--- Indexes for table `queue04`
---
-ALTER TABLE `queue04`
-  ADD PRIMARY KEY (`number`);
+ALTER TABLE `logindata`
+  ADD PRIMARY KEY (`roll`);
 
 --
 -- Indexes for table `subject`
@@ -684,10 +181,10 @@ ALTER TABLE `teacher`
   ADD PRIMARY KEY (`teacherId`);
 
 --
--- Indexes for table `users`
+-- Indexes for table `teaches`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`username`);
+ALTER TABLE `teaches`
+  ADD PRIMARY KEY (`subId`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
