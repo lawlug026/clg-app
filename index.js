c33bbbbbbcf711212814ed8d3a4ca9b64cc152bd
lawlug <ujwalgoyal.2@gmail.com>
2017-10-20 16:52:58 +0530

    login

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
 const sql = require('mysql');
-var basicAuthToken = require('express-basic-token');
+
+
 const app = express();
 //Initialising the basic token
-const Basictoken = 'cbpgec-a24-u26-n20-p21';
-// Creating connection to the database
+
 var con = sql.createConnection({
-	host:"localhost",
-	user:"root",
-	password:"notdefined",
-	database:"cbpgec"
+	host: "localhost",
+	user: "root",
+	password: "notdefined",
+	database: "cbpgec"
 });
 
-con.connect(function(err){
+con.connect(function (err) {
 	if (err) throw err;
 	console.log("connected");
-	});
+});
 
 
 
@@ -33,8 +33,8 @@ app.use(express.static(path.join(__dirname, 'dist')));
 // Set our api routes
 // app.use('/api', api);
 
-app.get('/test', function(req, res){
-    res.sendfile("form.html");
+app.get('/test', function (req, res) {
+	res.sendfile("form.html");
 })
 
 // app.post('/login', function(req, res){
@@ -49,73 +49,86 @@ app.get('/test', function(req, res){
 // 	}
 // 		)
 // 	}
-	
+
 
 // );
-var Bearer = 456;
-app.use('/login', basicAuthToken({ token: 'cbpgec' }));
-
-app.post('/login', function(req, res)
-	{
-		
-		var a1 = req.query.id;
-		var a2 = req.query.pass;
-		var a3 = req.query.basic;
-		// if (a3 == Basictoken)
-		// 	{
-				var stmt = "select * from logindata where roll = '"+a1+"';";
-				con.query(stmt, function(err, result)
-				{
-					if (err) {res.send("Invalid User");}
-					else 
-					{
-						console.log("Inside user checkig");
-						if(!result[0]){res.send("Invalid User");}
-						else
-						{
-							if (result[0].password == a2)
-							{
-								var roll = result[0].roll;
-								var name = result[0].password;	
+var Bearer = 0;
 
 
-								var stmt1 = "INSERT into bearer VALUES('"+Bearer+"', '"+roll+"');";
-								con.query(stmt1, function(err, result){
-									if (err) throw err;
-									else console.log("Insertion successful");
-								});
-								res.send("Login Success with" + a1 + '' + a2+ "Bearer Token" + Bearer);
-								Bearer+=1;
+app.post('/login', function (req, res, next) {
+
+	var a1 = req.query.id;
+	console.log(a1);
+	var a2 = req.query.pass;
+	console.log(a2);
+	var a4 = JSON.parse(JSON.stringify(req.headers));
+	console.log(a4.authorization);
+
+	// if (a3 == Basictoken)
+	// 	{
+	var stmt = "select * from logindata where roll = '" + a1 + "';";
+	con.query(stmt, function (err, result) {
+		if (err) { res.send("Invalid User"); }
+		else {
+			console.log("Inside user checkig");
+			if (!result[0]) { res.send("Invalid User frm db"); }
+			else {
+				if (result[0].password == a2) {
+					var roll = result[0].roll;
+					var name = result[0].password;
+					var stm = "select bear from bearer";
+					con.query(stm, function (err, res1) {
+						if (err) throw err;
+						else {
+							if (res1.length == 0) {
+								Bearer = 1;
 							}
 							else {
-								res.send("Wrong Credentials");
-								}
+								// var i = res.length();
+								console.log(res1.length);
+								var ln = res1.length - 1;
+								Bearer = res1[ln].bear + 1;
+							}
+							console.log(Bearer);
+
 						}
+					});
+					var stmt1 = "INSERT into bearer VALUES(" + Bearer + ", '" + roll + "');";
+					con.query(stmt1, function (err, result) {
+						if (err) throw err;
+						else console.log("Insertion successful");
+					});
 
-						
-					}
+					res.send("Login Success with" + a1 + '' + a2 + "Bearer Token" + Bearer);
+
 				}
-				)
-				
-		// 	}
-		// else{
-		// 	res.send("Wrong Authentication");
-		
-
-		
-	});
+				else {
+					res.send("Wrong Credentials");
+				}
+			}
 
 
-app.post('/form', function(req, res){
+		}
+	}
+	)
+
+	// 	}
+	// else{
+	// 	res.send("Wrong Authentication");
+
+
+
+});
+
+
+app.post('/form', function (req, res) {
 	var bear = req.query.bearer;
-	var stmt2 = "select * from logindata where token = '"+ bear+"';";
-	con.query(stmt2, function(err, data){
+	var stmt2 = "select * from logindata where token = '" + bear + "';";
+	con.query(stmt2, function (err, data) {
 		if (err) throw err;
-		else
-		{
-			if(!data[0]){res.send("User not available");}
-			else
-			{
+		else {
+			if (!data[0]) { res.send("User not available"); }
+			else {
 				console.log(data);
 				res.send(data);
 			}
@@ -123,41 +136,41 @@ app.post('/form', function(req, res){
 	})
 });
 
-var assData = 
+var assData =
 	{
-		'date':'12-10-2017',
-		'heading':'abc',
-		'ques':[
-			
+		'date': '12-10-2017',
+		'heading': 'abc',
+		'ques': [
+
 			{
-				'qu1':"jhsdb",
-				'op1':"12",
-				'op2':"13",
-				'opq':"321"
+				'qu1': "jhsdb",
+				'op1': "12",
+				'op2': "13",
+				'opq': "321"
 			},
-			
+
 			{
-				'qu1':"jhsdb",
-				'op1':"12",
-				'op2':"13",
-				'opq':"321"
+				'qu1': "jhsdb",
+				'op1': "12",
+				'op2': "13",
+				'opq': "321"
 			},
-			
+
 			{
-				'qu1':"jhsdb",
-				'op1':"12",
-				'op2':"13",
-				'opq':"321"
+				'qu1': "jhsdb",
+				'op1': "12",
+				'op2': "13",
+				'opq': "321"
 			},
-			
+
 			{
-				'qu1':"jhsdb",
-				'op1':"12",
-				'op2':"13",
-				'opq':"321"
+				'qu1': "jhsdb",
+				'op1': "12",
+				'op2': "13",
+				'opq': "321"
 			},
-			],
-			'ans':'q1'
+		],
+		'ans': 'q1'
 	}
 
 
@@ -165,71 +178,68 @@ var assData =
 ////////Assignment////////////////////
 
 var MysqlJson = require('mysql-json');
-  var mysqlJson = new MysqlJson({
-    host:'localhost',
-    user:'root',
-    password:'notdefined',
-    database:'cbpgec'
-  });
+var mysqlJson = new MysqlJson({
+	host: 'localhost',
+	user: 'root',
+	password: 'notdefined',
+	database: 'cbpgec'
+});
 var temp = 0;
 var assID;
-app.post('/assignment', function(req, res, next){
-	assID = 'ass'+temp;
+app.post('/assignment', function (req, res, next) {
+	assID = 'ass' + temp;
 
-	
+
 
 	var bear = req.query.bear;
-	var stmt2 = "select * from bearer where bear = '"+ bear+"';";
-	con.query(stmt2, function(err, data){
+	var stmt2 = "select * from bearer where bear = '" + bear + "';";
+	con.query(stmt2, function (err, data) {
 		if (err) throw err;
-		else
-		{
-			if(!data[0]){res.send("Teacher not available Or session expired...Login Again");}
-			else
-			{
-				console.log(data[0].roll+"submitted the assignment");
-				
+		else {
+			if (!data[0]) { res.send("Teacher not available Or session expired...Login Again"); }
+			else {
+				console.log(data[0].roll + "submitted the assignment");
+
 
 				mysqlJson.insert('assignment', {
-				assid:assID,
-			    date:assData.date,
-			    heading:assData.heading,
-			    ans:assData.ans				   
-				  }, function(err, response) {
-				  	console.log("checkpoint1");
-				    if (err) throw err;
-				    console.log(response);
-				  });
-					
-					var stmt5 = 'create table ' + assID + ' (ques varchar(255), op1 char(20), op2 char(20));';
-					console.log(stmt5);
-					console.log("statmenet paasesed");
-					con.query(stmt5, function(err, result){
-						if (err) throw err;
-						console.log("Table created");
-						next();
-						res.send("success");
-					})					
+					assid: assID,
+					date: assData.date,
+					heading: assData.heading,
+					ans: assData.ans
+				}, function (err, response) {
+					console.log("checkpoint1");
+					if (err) throw err;
+					console.log(response);
+				});
+
+				var stmt5 = 'create table ' + assID + ' (ques varchar(255), op1 char(20), op2 char(20));';
+				console.log(stmt5);
+				console.log("statmenet paasesed");
+				con.query(stmt5, function (err, result) {
+					if (err) throw err;
+					console.log("Table created");
+					next();
+					res.send("success");
+				})
 			}
 		}
 	})
-}, function(){
-						for(i=0; i<assData.ques.length;i++)
-		{
-						mysqlJson.insert(assID, {
-				ques:assData.ques[i].qu1,
-					    op1:assData.ques[i].op1,
-					    op2:assData.ques[i].op2
-					   
-					  }, function(err, response) {
-					    if (err) throw err;
-					    console.log(response);
-					  });
-					}
-					temp+=1;
-					
+}, function () {
+	for (i = 0; i < assData.ques.length; i++) {
+		mysqlJson.insert(assID, {
+			ques: assData.ques[i].qu1,
+			op1: assData.ques[i].op1,
+			op2: assData.ques[i].op2
 
-					})
+		}, function (err, response) {
+			if (err) throw err;
+			console.log(response);
+		});
+	}
+	temp += 1;
+
+
+})
 
 
 
@@ -243,7 +253,7 @@ app.post('/assignment', function(req, res, next){
 
 // // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+	res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 /**
